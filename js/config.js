6eaeba20857e3951d41597156a1794defdaa2669
hiwantan-commits <hi.wantan@gmail.com>
2026-08-25 15:01:16 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// js/config.js
+// Pusat konfigurasi aplikasi (Hanya atur di sini untuk seluruh halaman)
+
+const CONFIG = {
+    // Masukkan Spreadsheet ID Anda di sini (di antara /d/ dan /edit pada link Google Sheets)
+    SPREADSHEET_ID: "1259Lj222pmtMthIQ6PjuknpVlAMGjlsIyNcISENgYUU",
+    
+    // Masukkan URL Web App Google Apps Script Anda di sini
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx2bCxIwIGsHeUSaEs5rXBFMg4chXhDY4AgRtbHrnWkC-3hcrwxDQVSosSnDpPSQMSKeg/exec",
+    
+    // Nama sheet database transaksi
+    SHEET_NAME: "Database_Transaksi"
+};
